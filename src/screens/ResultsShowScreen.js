20a5c2457dbac7bf46a2860217c6453bdd8ecf66
Lihem <mlhcsgn10@gmail.com>
2021-06-10 11:13:26 +0300

    Restaurant Search App finished for now

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React , {useState, useEffect} from 'react'
+import {View, Text, StyleSheet , FlatList, Image} from 'react-native'
+import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({navigation}) => {
+    const [result,setResult] = useState(null)
     const id = navigation.getParam('id')       //navigation ile parametreleri alma
+
+    const getResult = async (id)=>{
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data)
+    }
+    useEffect (()=>{
+        getResult(id)
+    },[]) //bos array useEffectin sadece bir kere calismasi icin
+
+    if(!result){
+        return null
+    }
+
     return <View>
-        <Text>resultsssss</Text>
+        <Text>{result.name}</Text>
+        <FlatList
+        data = {result.photos}
+        keyExtractor = {(photo) => photo}
+        renderItem = {({item}) => {
+            return <Image style={styles.imageStyle} source={{uri: item}} />
+        }} 
+        />
     </View>
 }
 
 const styles = StyleSheet.create({
-
+    imageStyle:{
+        height:200,
+        width:300,
+    }
 })
 
 export default ResultsShowScreen
